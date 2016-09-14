ef2bba88bb375693f0d2232f6d88ee5696db5cd5
vishaldodiya <vdodiya03@gmail.com>
2016-09-14 19:09:53 +0530

    Updated Index.php

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <html>
-<head>
-<title>WEBPAGE in TABLE</TITLE>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
 
-<body>
-	
-	<table>
-	<td border="1">
-	<tr><center><h1>WELCOME TO GOA SINGHAM !</h1><center></tr>
-	</table>
-	
-	</body>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Vishal Dodiya</h1>
+                <br>
+                <h2>RtCamp Assignment:</h2>
+                <br>
+            </div>
+            <br>
+            <div class="jumbotron">
+                <button class="btn btn-primary">Twiter Login</button>
+            </div>
+            <br>
+            <div class="jumbotron">
+
+            </div>
+        </div>
+
+    </body>            
 </html>
+<?php
+
+?>
