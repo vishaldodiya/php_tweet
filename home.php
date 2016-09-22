8536b2c377ae7b50db8bd73e19e736089d587352
vishaldodiya <vdodiya03@gmail.com>
2016-09-22 12:30:39 +0530

    1.11

--- home.php
+++ home.php
@@ -152,12 +152,18 @@
                     </div>
                     <div class="col-sm-6" >
                        <div class='carousel slide' id='myCarousel'>
+                       <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
                         <div class='carousel-inner' role='listbox' id="tweet">
 
 
-                        </div> 
-
-                        <a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>
+                        
+                        </div>
+                            <a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>
                                 <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
                                 <span class='sr-only'>Previous</span>
                             </a>
@@ -166,7 +172,9 @@
                                 <span class='sr-only'>Next</span>
                             </a>
 
-                      </div>   
+                      </div> 
+                        
+
                     </div>
                 </div>
 
