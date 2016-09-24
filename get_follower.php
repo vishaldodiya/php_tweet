1d2a5e66a190ab04a0e8f4441588db149a98bb78
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 17:27:26 +0530

    cout 100

--- get_follower.php
+++ get_follower.php
@@ -7,7 +7,7 @@
     if(isset($_SESSION['access_token'])){
             $access_token = $_SESSION['access_token'];
             $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
-            $count = 5000;
+            $count = 100;
 
             $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi'));
 
