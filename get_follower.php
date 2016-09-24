2f2bd7fe07e6071f031486740c178ced3efe9b7c
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 17:26:08 +0530

    updated file

--- get_follower.php
+++ get_follower.php
@@ -2,18 +2,19 @@
 
     session_start();
     require "autoload.php";
-    use Abraham\TwitterOAuth\TwitterOAuth
-
+    use Abraham\TwitterOAuth\TwitterOAuth;
+    
     if(isset($_SESSION['access_token'])){
-        $access_token = $_SESSION['access_token'];
-        $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
+            $access_token = $_SESSION['access_token'];
+            $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
+            $count = 5000;
 
-        $count = 5000;
+            $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi'));
 
-        $followers = $connection->get('followers/ids', ['count' => $count, 'screen_name' => 'narendramodi']);
+            echo "<pre>";
+            print_r($followers);
+            echo "</pre>";
+    }else{
 
-        echo "<pre>";
-        print_r($followers);
-        echo "</pre>";  
     }
 ?>
