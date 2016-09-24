f0caf253fe64ad13864fcc4c943d698028cbc391
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 17:39:09 +0530

    upadtes

--- get_follower.php
+++ get_follower.php
@@ -12,13 +12,14 @@
 
             while($cursor != 0){
                 $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => $cursor));
+                echo "<pre>";
+                print_r($followers);
+                echo "</pre>";
                 $cursor = $followers->next_cursor;
             }
             
 
-            echo "<pre>";
-            print_r($followers);
-            echo "</pre>";
+            
     }else{
 
     }
