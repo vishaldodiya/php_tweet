b2fe1f5dc6b5805afe31407dc0a98b67e3a67cee
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 17:37:23 +0530

    taking risk

--- get_follower.php
+++ get_follower.php
@@ -8,8 +8,13 @@
             $access_token = $_SESSION['access_token'];
             $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
             $count = 5000;
+            $cursor = -1;
 
-            $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => '-1'));
+            while($cursor != 0){
+                $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => $cursor));
+                $cursor = $followers->next_cursor;
+            }
+            
 
             echo "<pre>";
             print_r($followers);
