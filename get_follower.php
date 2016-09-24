e7d26ad0ac991faf498b2fe9f69f4bab8dd90343
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 20:38:55 +0530

    Update get_follower.php

--- get_follower.php
+++ get_follower.php
@@ -7,17 +7,17 @@
     if(isset($_SESSION['access_token'])){
             $access_token = $_SESSION['access_token'];
             $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
-            $count = 5000;
+            $count = 100;
             $cursor = -1;
             $follow_array;
-            while($cursor != 0){
+            //while($cursor != 0){
                 
                 
                 $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => $cursor));
                 $follow_array = json_encode($followers,true);
-                $follow_array = array_merge($follow_array, $followers);
-                $cursor = $followers->next_cursor;
-            }
+              //  $follow_array = array_merge($follow_array, $followers);
+            //    $cursor = $followers->next_cursor;
+            //}
             
 
             echo "<pre>";
