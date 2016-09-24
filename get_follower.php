07b29abf8a49dc926a252dbe3a9af0f3c222a1d0
vishaldodiya <vdodiya03@gmail.com>
2016-09-24 17:48:59 +0530

    changed array

--- get_follower.php
+++ get_follower.php
@@ -9,17 +9,17 @@
             $connection = new TwitterOAuth($_SESSION['consumer_key'], $_SESSION['consumer_secret'], $access_token['oauth_token'], $access_token['oauth_token_secret']);
             $count = 5000;
             $cursor = -1;
-
+            $follow_array = array();
             while($cursor != 0){
                 $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => $cursor));
-                echo "<pre>";
-                print_r($followers);
-                echo "</pre>";
+                $follow_array.push($followers);
                 $cursor = $followers->next_cursor;
             }
             
 
-            
+            echo "<pre>";
+            print_r($follow_array);
+            echo "</pre>";
     }else{
 
     }
