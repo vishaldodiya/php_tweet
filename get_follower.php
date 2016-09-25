d4ab807f89ada4e4f63c84d1fbebceb9e731a2e5
vishaldodiya <vdodiya03@gmail.com>
2016-09-25 11:05:50 +0530

    Update get_follower.php

--- get_follower.php
+++ get_follower.php
@@ -10,19 +10,21 @@
             $count = 100;
             $cursor = -1;
             $follow_array = array();
-            //while($cursor != 0){
+            while($cursor != 0){
                 
                 
                 $followers = $connection->get('followers/ids',array('count' => $count, 'screen_name' => 'narendramodi', 'cursor' => $cursor));
-                $follow_array = json_encode($followers,true);
-                $follow_array = array_merge($follow_array, $follow_array);
-                $cursor = $followers->next_cursor;
-            //}
-            
-
-            echo "<pre>";
+                echo "<pre>";
             print_r($follow_array);
             echo "</pre>";
+                //if($folloers->
+                //$follow_array = json_encode($followers,true);
+                //$follow_array = array_merge($follow_array, $follow_array);
+                $cursor = $followers->next_cursor_str;
+            }
+            
+
+            
     }else{
 
     }
